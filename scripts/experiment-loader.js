5b0a9b1811bcb68c12f6e2d20abcbf0767bcc025
Xinyi Feng <xfeng@adobe.com>
2025-07-15 16:01:17 -0700

    pass config to showexperimentationrail

--- scripts/experiment-loader.js
+++ scripts/experiment-loader.js
@@ -34,7 +34,7 @@ export async function runExperimentation(document, config) {
  * @param {Document} document The document object.
  * @returns {Promise<void>} A promise that resolves when the experimentation module is loaded.
  */
-export async function showExperimentationRail(document) {
+export async function showExperimentationRail(document, config) {
   if (!isExperimentationEnabled()) {
     return null;
   }
